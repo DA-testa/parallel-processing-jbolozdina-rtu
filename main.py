f42296d56834ed7384233b102ff20c649e23cf8d
Jegors Bolozdiņa <125827886+jbolozdina-rtu@users.noreply.github.com>
2023-03-18 20:39:20 +0000

    rewrite

--- main.py
+++ main.py
@@ -7,33 +7,12 @@ def parallel_processing(n, m, data):
     threads = [(0, i) for i in range(n)]
 
     for i in range(m):
-        ti = data[i]
-        start_time, thread_idx = threads[0]
-        output.append((thread_idx, start_time))
-        threads[0] = (start_time+ti, thread_idx)
-        j = 0
-        while True:
-            left_child = 2*j + 1
-            right_child = 2*j + 2
-            if left_child >= n:
-                break
-            min_child = left_child
-            if right_child < n and threads[right_child][0] < threads[left_child][0]:
-                min_child = right_child
-            if threads[min_child][0] < threads[j][0]:
-                threads[j], threads[min_child] = threads[min_child], threads[j]
-                j = min_child
-            else:
-                break
+        next_thread = min(threads)
+        threads[next_thread[1]] = (next_thread[0] + data[i], next_thread[1])
+        output.append((next_thread[1], next_thread[0]))
 
     return output
 
-
-    def print_grid(grid):
-        for j in range(len(grid[0])):
-            for i in range(len(grid)):
-                print(i, j)
-                print(grid[i][j])
 
 
 def main():
@@ -41,10 +20,9 @@ def main():
     data = list(map(int, input().split()))
 
     result = parallel_processing(n, m, data)
-    
-    # for thread_idx, start_time in result:
-    #     print(thread_idx, start_time)
-    print_grid(n, m, result)
+
+    for i in range(m):
+        print(result[i][0], result[i][1])
 
 
 
